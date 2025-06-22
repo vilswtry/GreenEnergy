546b56d74b2556cab2fbb342229e7886f7c34068
val <133932492+vilswtry@users.noreply.github.com>
2025-06-22 15:47:39 -0400

    se añade nuevamente generacionReportes

---
+++ generacionReportes/src/main/java/com/GreenEnergy/generacionReportes/GeneracionReportesApplication.java
@@ -0,0 +1,13 @@
+package com.GreenEnergy.generacionReportes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneracionReportesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneracionReportesApplication.class, args);
+	}
+
+}
