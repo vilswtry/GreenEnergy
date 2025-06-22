483fadc6d7f208d3f0aca7e98e457aac82ee9a4a
val <valefon222@gmail.com>
2025-06-22 15:45:35 -0400

    se elimina atencionCliente

--- atencionClientes/src/main/java/com/GreenEnergy/atencionClientes/repository/SolicitudRepository.java
+++
@@ -1,7 +0,0 @@
-package com.GreenEnergy.atencionClientes.repository;
-
-import com.GreenEnergy.atencionClientes.model.Solicitud;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SolicitudRepository extends JpaRepository<Solicitud, Long> {
-}
