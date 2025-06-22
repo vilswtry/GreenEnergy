3e1c1ea29de48a970f0cff2c29138fa30a943bfe
val <133932492+vilswtry@users.noreply.github.com>
2025-06-22 00:10:24 -0400

    se añade el microservicio soporteTecnico

---
+++ soporteTecnico/src/main/java/com/GreenEnergy/soporteTecnico/SoporteTecnicoApplication.java
@@ -0,0 +1,13 @@
+package com.GreenEnergy.soporteTecnico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoporteTecnicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoporteTecnicoApplication.class, args);
+	}
+
+}
---
+++ soporteTecnico/src/test/java/com/GreenEnergy/soporteTecnico/SoporteTecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GreenEnergy.soporteTecnico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoporteTecnicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
